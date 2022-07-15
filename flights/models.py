c06631eaf6bba5f049889cea16f7968d2f7ef752
Lucas Mariano <lucas.svmariano@gmail.com>
2022-07-15 14:04:40 -0300

    estado incial do projeto

---
+++ flights/models.py
@@ -0,0 +1,23 @@
+from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+
+#herdando do módulo models as características  da classe 'Model'
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination} with duration {self.duration} min"
+
+
