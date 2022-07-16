d47d5135dca05c1b755551296e7e9732bf9be24a
Lucas Mariano <lucas.svmariano@gmail.com>
2022-07-16 18:54:30 -0300

    aplicação semifinalizada

--- flights/admin.py
+++ flights/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 
 # Register your models here.
-admin.site.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
+
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
+admin.site.register(Passenger, PassengerAdmin)
